084fd43c7888c4ec5b05cc5b6ccfa244b4f9e3ed
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-02-04 03:15:21 +0900

    Swiper 플러그인 추가

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * 슬라이드
+ * https://swiperjs.com/
+ */
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+});
